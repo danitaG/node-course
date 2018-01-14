f0120655df3eeb76b31e6401df9e3b8933f3db4c
Danita Gnanaraj <danita.gnanaraj-cic.netherldands@ibm.com>
2018-01-14 17:58:46 +0100

    changed text from port to PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 var app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //to set up partials
 hbs.registerPartials(__dirname + '/views/partials')
